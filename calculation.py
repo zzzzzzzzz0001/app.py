301205db807d5ad34893b2ccf9b2ffbfd48c4e84
zzzzzzzzz0001 <02041607904z@gmail.com>
2025-03-18 16:37:51 +1300

    Add files via upload

---
+++ calculation.py
@@ -0,0 +1,23 @@
+#Here we have the same calculation program from our previous activities
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "number 2 cannot be 0"
+    else:
+        return num1 / num2
+
+
+def power(num1, num2):
+    return num1 ** num2
